b8262a4615e1528688d6de0ed46e830c903e1bfc
Cameron Gray <cameron.gray@senecacollege.ca>
2017-07-25 16:41:13 -0400

    Fixed Milestone #3 project file to include empty source file and tester

---
+++ MS3/144_ms3.c
@@ -0,0 +1,13 @@
+// -------------------------------------------
+//  Name:
+//  ID:
+//  Email:
+//  Description: Milestone #3
+// -------------------------------------------
+
+#include <stdio.h>
+
+// Copy your milestone #2 source code below
+// - Add new function prototypes and definitions
+//   for milestone #3 as per the directions
+
